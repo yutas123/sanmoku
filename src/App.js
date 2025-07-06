197b14ea882f1c7575665a4cf22be2fe1c5cf743
小林勇太崇 <kobayashiyut@pivot.jp>
2025-07-06 10:14:08 +0900

    果物リスト

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import { useState } from "react";
+
 function FruitList() {
-  const fruits = ["りんご", "バナナ", "みかん"];
+  const [fruits, setFruits] = useState(["りんご", "バナナ", "みかん"]);
+
+  const handleAddFruit = () => {
+    setFruits([...fruits, "もも"]);
+  };
 
   return (
-    <ul>
-      {fruits.map((fruit, index) => (
-        <li key={index}>{fruit}}</li>
-      ))}
-    </ul>
+    <div>
+      <h2>🍎 果物リスト</h2>
+      <ul>
+        {fruits.map((fruit, index) => (
+          <li key={index}>{fruit}</li>
+        ))}
+      </ul>
+      <button onClick={handleAddFruit}>果物を追加</button>
+    </div>
   );
 }
 
-export default App;
+
+export default FruitList;
