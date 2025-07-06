defecdeb62910b964b6fb5ea3dc810af69ec9df2
小林勇太崇 <kobayashiyut@pivot.jp>
2025-07-06 09:56:27 +0900

    wip

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
-import { useState } from "react";
-function MyComponent() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+function FruitList() {
+  const fruits = ["りんご", "バナナ", "みかん"];
 
   return (
-    <div>
-      {isLoggedIn ? (
-        <h2>ようこそ、ログイン済みユーザーさん！</h2>
-      ) : (
-        <h2>ログインしてください。</h2>
-      )}
-
-      <button onClick={() => setIsLoggedIn(!isLoggedIn)}>
-        状態を切り替える
-      </button>
-    </div>
+    <ul>
+      {fruits.map((fruit, index) => (
+        <li key={index}>{fruit}}</li>
+      ))}
+    </ul>
   );
 }
 
-export default MyComponent;
+export default App;
