56e3ef900e0d39a5395e555a6725f3f7e9acc724
小林勇太崇 <kobayashiyut@pivot.jp>
2025-07-06 09:48:38 +0900

    ログイン状態切り替え

--- src/App.js
+++ src/App.js
@@ -1,54 +1,20 @@
-// 親コンポーネント
 import { useState } from "react";
-
-function Parent() {
-  const [message, setMessage] = useState("（まだメッセージがありません）");
-
-  function handleNotify(newMessage) {
-    setMessage(newMessage);
-  }
+function MyComponent() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
     <div>
-      <h2>📩 受信メッセージ：</h2>
-      <p>{message}</p>
-      <ChildA sendToParent={handleNotify} />
-      <ChildB message={message} />
+      {isLoggedIn ? (
+        <h2>ようこそ、ログイン済みユーザーさん！</h2>
+      ) : (
+        <h2>ログインしてください。</h2>
+      )}
+
+      <button onClick={() => setIsLoggedIn(!isLoggedIn)}>
+        状態を切り替える
+      </button>
     </div>
   );
 }
 
-// 子コンポーネント（フォーム入力あり）
-function ChildA({sendToParent}) {
-  const [inputValue, setInputValue] = useState("");
-
-  function handleClick() {
-    sendToParent(inputValue);
-    setInputValue(""); // 送信後に入力欄をリセット
-  }
-
-  return (
-    <div>
-      子コンポーネントA
-      <input
-        type="text"
-        placeholder="親に送りたいメッセージ"
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-      />
-      <button onClick={handleClick}>送信</button>
-    </div>
-  );
-}
-
-function ChildB({ message }) {
-  console.log("受け取ったprops:", message);
-  return (
-    <div>
-      <h3>子コンポーネントB</h3>
-      <p>親からのメッセージ: {message}</p>
-    </div>
-  );
-} 
-
-export default Parent;
+export default MyComponent;
