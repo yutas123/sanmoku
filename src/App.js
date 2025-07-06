1771b6ec39ba76924f6be578dd2e993b6ae4dd00
小林勇太崇 <kobayashiyut@pivot.jp>
2025-07-06 20:16:11 +0900

    Objectについて学び

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { useState } from "react";
 
-const Object = {
+const person = {
   name: "John Doe",
   age: 30,
   country: "USA"
 }
 
-const objectKeys = Object.keys(Object);
-console.log(objectKeys); // ["name", "age", "country"]
+const objectKeys = Object.keys(person);
+console.log(Object);
+// console.log(objectKeys); // ["name", "age", "country"]
 
 export default ContactForm;
