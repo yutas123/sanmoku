3c604a7f07a8a54ea2b10a446648f0dd1095fca7
小林勇太崇 <kobayashiyut@pivot.jp>
2025-09-16 17:29:21 +0900

    独立ver:stateをMyButtonが保持する場合

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
+import { useState } from 'react';
+
 export default function MyApp() {
-  const products = [
-    { title: 'Cabbage', id: 1 },
-    { title: 'Garlic', id: 2 },
-    { title: 'Apple', id: 3 },
-  ];
-  
-  // ifにはreturnを書くのを忘れずに
-  // 条件(三項)演算子は暗黙的にreturnされるので必要ない  
-  const listItems = products.map(product => {
-    if(product.id >= 2){
-      return <li key={product.id}>{product.title}</li>;
-    } else {
-      return null;
-    }
-  }
-  );
-  
   return (
-    <ul>{listItems}</ul>
+    <div>
+      <h1>Counters that update separately</h1>
+      <MyButton />
+      <MyButton />
+    </div>
+  );
+}
+
+function MyButton() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    console.log(count);
+    setCount(count + 1);
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Clicked {count} timesss
+    </button>
   );
 }
