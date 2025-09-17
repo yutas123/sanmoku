bbaf9a818f4fafc15639920b893abbdc7cca13c6
小林勇太崇 <kobayashiyut@pivot.jp>
2025-09-17 14:12:24 +0900

    clickが紐づいている状態

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import { useState } from 'react';
 
-export default function MyApp() {
+export default function App() {
   const [count, setCount] = useState(0);
-
-  function handleClick() { 
-    setCount(count + 1); 
+  function CountAdd(){
+    setCount(count + 1);
   }
 
   return (
     <div>
-      <h1>Counters that update separately</h1>
-      <MyButton count={count} onClick={handleClick}/>
-      <MyButton count={count} onClick={handleClick}/>
+      <button onClick={CountAdd}> click {count} 回数</button>
+      <button onClick={CountAdd}> click {count} 回数</button>
     </div>
   );
 }
 
-function MyButton({count, onClick}) {
-  return (
-    <button onClick={handleClick}>
-      Clicked {count} timesss
-    </button>
-  );
-}
+  
