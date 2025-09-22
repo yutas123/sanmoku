934617fe4e38ebf4685f3cd8c5e147d536a32a54
小林勇太崇 <kobayashiyut@pivot.jp>
2025-09-22 15:53:44 +0900

    修正

--- src/App.js
+++ src/App.js
@@ -37,9 +37,9 @@ function Board({ xIsNext, squares, onPlaysss }) {
     <>
       <div className="status">{status}</div>
       <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(1)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[3]} onSquareClick={() => handleClick(2)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
         <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
